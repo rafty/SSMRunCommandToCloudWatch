b9188b6f45b6c3b5ef3c314c9257ca5c9600ba05
takashi <yagita.takashi@gmail.com>
2022-05-02 11:00:07 +0900

    Initial commit

---
+++ tests/unit/test_ssm_run_command_to_cloud_watch_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from ssm_run_command_to_cloud_watch.ssm_run_command_to_cloud_watch_stack import SsmRunCommandToCloudWatchStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in ssm_run_command_to_cloud_watch/ssm_run_command_to_cloud_watch_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = SsmRunCommandToCloudWatchStack(app, "ssm-run-command-to-cloud-watch")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
